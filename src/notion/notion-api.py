17bba2fa6030cc196aacb55cc48e5804f7404827
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2023-07-23 20:27:21 +0530

    refactored Code, added NSEtl class

--- src/notion/notion-api.py
+++ src/notion/notion-api.py
@@ -2,50 +2,49 @@
 
 import os
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 
-load_dotenv()
+class NSEtl():
 
-NOTION_API = os.environ.get('NOTION_API')
-DATABASE_ID = os.environ.get('DATABASE_ID')
+    def __init__(self):
+        load_dotenv()
 
-# API header
-headers = {
-    "Authorization": f"Bearer {NOTION_API}",
-    "accept": "application/json",
-    "Notion-Version": "2022-06-28"
-}
-
-# Get database data
-def get_pages(num_pages=None):
-    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
-
-    page_size = 100 if num_pages is None else num_pages
-
-    payload = {"page_size": page_size}
-    resp = requests.post(url, json=payload, headers=headers)
+        self._NOTION_API = os.environ.get('NOTION_API')
+        self._DATABASE_ID = os.environ.get('DATABASE_ID')
+        self.PAGE_SIZE = 100
+        
+        # API header
+        self.headers = {
+            "Authorization": f"Bearer {self._NOTION_API}",
+            "accept": "application/json",
+            "Notion-Version": "2022-06-28"
+        }
     
-    if resp.status_code == 200:
+    def extract(self):
+        # Get database data
+        url = f"https://api.notion.com/v1/databases/{self._DATABASE_ID}/query"
 
-        data = resp.json()['results']
+        payload = {"page_size": self.PAGE_SIZE}
+        resp = requests.post(url, json=payload, headers=self.headers)
 
-        for attributes in data:
-            # print(attributes)
-            question = attributes['properties']['Questions']['title'][0]['text']['content']
-            tags = attributes['properties']['Tags']['select']['name']
-            created_time = attributes['created_time']
-            platform = attributes['properties']['Platform']['select']['name']
-            company = attributes['properties']['Company']['multi_select']
+        if resp.status_code == 200:
+            data = resp.json()['results']
 
-            ndict = {
-                'Question Title': question,
-                'Tags': tags,
-                'Created Time': created_time,
-                'Platform Solved': platform,
-                'Company Tag': company
-            }
+            for attributes in data:
+                question = attributes['properties']['Questions']['title'][0]['text']['content']
+                difficulty = attributes['properties']['Tags']['select']['name']
+                created_time = datetime.strptime(attributes['created_time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+                platform = attributes['properties']['Platform']['select']['name']
+                company = [{'Company': item["name"]} for item in attributes['properties']['Company']["multi_select"]]
 
-            print(ndict)
-            print()
+                ndict = {
+                    'Question Title': question,
+                    'Dfficulty leve': difficulty,
+                    'Created Time': created_time,
+                    'Platform Solved': platform,
+                    'Company Tag': company
+                }
 
-get_pages(100)
+                print(ndict)
+                print()
