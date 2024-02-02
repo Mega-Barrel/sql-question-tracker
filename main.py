992b1fbbe192b1139ff59f6accd3d295cbdfd3d9
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2024-02-02 22:14:56 +0530

    main code to call NotionWrapper

---
+++ main.py
@@ -0,0 +1,9 @@
+"""Test call"""
+from src.notion.notion_api import NotionWrapper
+
+if __name__ == '__main__':
+    nw = NotionWrapper()
+    data = nw.extract()
+    nw.load_data(
+        raw_data=data
+    )
